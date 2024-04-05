437a339e6ce44f66417b73abba12c3c71e891a3b
Dreamer <2397952362@qq.com>
2024-04-05 16:50:39 +0800

    添加用户注册信息校验以及其异常处理逻辑

--- src/main/java/com/dreamer/demoproject/controller/UserController.java
+++ src/main/java/com/dreamer/demoproject/controller/UserController.java
@@ -3,18 +3,21 @@ package com.dreamer.demoproject.controller;
 import com.dreamer.demoproject.pojo.Result;
 import com.dreamer.demoproject.pojo.User;
 import com.dreamer.demoproject.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("/register")
-    public Result register(String username ,String password){
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username ,@Pattern(regexp = "^\\S{5,16}$") String password){
         //查询用户名是否重复
         User u=userService.findByUserName(username);
         if(u==null){
